d3da3411d3cb0d440638675807b68d44f015f03c
wwwbrunagon <wwwbrunagon@gmail.com>
2023-05-22 17:23:39 -0300

    feat: DTO Validation

--- src/user/user.controllers.ts
+++ src/user/user.controllers.ts
@@ -4,16 +4,20 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Put,
 } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 
 @Controller('users')
 export class UserController {
   @Post()
-  async create(@Body() body) {
-    return { body };
+  async create(@Body() { name, email, password }: CreateUserDTO) {
+    return { name, email, password };
   }
 
   @Get()
@@ -27,25 +31,35 @@ export class UserController {
   }
 
   @Put(':id')
-  async update(@Body() body, @Param() params) {
+  async update(
+    @Body() { name, email, password }: UpdatePutUserDTO,
+    @Param() params,
+  ) {
     return {
       method: 'put',
-      body,
+      name,
+      email,
+      password,
       params,
     };
   }
 
   @Patch(':id')
-  async updatePartial(@Body() body, @Param() params) {
+  async updatePartial(
+    @Body() { name, email, password }: UpdatePatchUserDTO,
+    @Param() params,
+  ) {
     return {
       method: 'patch',
-      body,
+      name,
+      email,
+      password,
       params,
     };
   }
 
   @Delete(':id')
-  async delete(@Param() params) {
-    return { params };
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return { id };
   }
 }
