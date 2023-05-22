fe380c92ce3a1cd66f70aa5389a278b16d97981b
wwwbrunagon <wwwbrunagon@gmail.com>
2023-05-22 15:41:07 -0300

    CRUD

--- src/user/user.controllers.ts
+++ src/user/user.controllers.ts
@@ -1,9 +1,42 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   @Post()
   async create(@Body() body) {
     return { body };
+  }
+
+  @Get()
+  async list() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async show(@Param() params) {
+    return { user: {}, params };
+  }
+
+  @Put(':id')
+  async update(@Body() body, @Param() params) {
+    return {
+      method: 'put',
+      body,
+      params,
+    };
+  }
+
+  @Patch(':id')
+  async updatePartial(@Body() body, @Param() params) {
+    return {
+      method: 'patch',
+      body,
+      params,
+    };
+  }
+
+  @Delete(':id')
+  async delete(@Param() params) {
+    return { params };
   }
 }
