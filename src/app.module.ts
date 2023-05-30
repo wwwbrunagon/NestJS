746f77581fc496d5cdf749b8d76db5c0b421ccab
wwwbrunagon <wwwbrunagon@gmail.com>
2023-05-30 13:44:52 -0300

    quickly creating a CRUD controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [UserModule],
-  controllers: [AppController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
